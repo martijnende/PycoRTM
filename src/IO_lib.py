Fixes in BC, code cleanup, more benchmark tests There probably is a bug in the temperature gradient BC (important for advection term)

src/IO_lib.py
@@ -1,34 +1,79 @@
 import numpy as np
 import pandas as pd
+from time import time
 
 
 class IO:
 
-    def __init__(self, IO_file):
-        self.IO_file = IO_file
-        self.IO_cols = ("t", "n", "r", "theta", "x", "y", "T", "u_r", "u_theta")
+    def __init__(self):
+        # Time series file
+        self.OT_cols = ("t", "T_mean", "T_max")
+
+        # Snapshot file
+        self.OX_cols = ("t", "n", "r", "theta", "x", "y", "T", "u_r", "u_theta")
         pass
 
-    def create_IO_file(self):
-        df = pd.DataFrame(columns=self.IO_cols)
-        with open(self.IO_file, "w") as f:
-            df.to_csv(f, index=False)
+    def create_IO_files(self):
+
+        if self.IO_file_time is not None:
+            df = pd.DataFrame(columns=self.OT_cols)
+            with open(self.IO_file_time, "w") as f:
+                df.to_csv(f, index=False)
+
+        if self.IO_file_snap is not None:
+            df = pd.DataFrame(columns=self.OX_cols)
+            with open(self.IO_file_snap, "w") as f:
+                df.to_csv(f, index=False)
         return True
 
     def read(self):
-        print("Reading data")
-        df = pd.read_csv(self.IO_file, names=self.IO_cols, header=0)
-        self.data = df
-        return df
 
-    def write(self, t, T, u_r, u_theta):
+        if self.IO_file_time is not None:
+            print("Reading time series data")
+            df = pd.read_csv(self.IO_file_time, names=self.OT_cols, header=0)
+            self.ot_data = df
+
+        if self.IO_file_snap is not None:
+            print("Reading snapshot data")
+            df = pd.read_csv(self.IO_file_snap, names=self.OX_cols, header=0)
+            self.ox_data = df
+        return True
+
+    def screen_write(self, t, dt, T, u_r, u_theta):
+        T_mean = self.compute_mean(T)
+        t_elapsed = time() - self.t_wall_start
+        tmax = self.tmax
+        ETA = (t_elapsed / t) * (tmax - t) / 60
+        print(
+            "t = %.1f kyr \t dt = %.2e yr \t Tmax = %.1f K \t Tmean = %.1f K \t vmax = %.3e m/s \t ETA = %.1f min" %
+            (1e-3 * t / self.t_yr, dt / self.t_yr, T.max(), T_mean,
+             np.max(np.sqrt(u_r ** 2 + u_theta ** 2)), ETA)
+        )
+        return True
+
+    def OT_file_write(self, t, T):
+
+        T_max = T.max()
+        T_mean = self.compute_mean(T)
+
+        output_dict = {
+            "t": t, "T_mean": T_mean, "T_max": T_max,
+        }
+
+        output_df = pd.DataFrame(output_dict, columns=self.OT_cols, index=[0])
+        with open(self.IO_file_time, "a") as f:
+            output_df.to_csv(f, header=False, index=False)
+
+        return True
+
+    def OX_file_write(self, t, T, u_r, u_theta):
 
         N_r = self.N_r
         N_theta = self.N_theta
 
         n = np.arange(N_r*N_theta)
         j = n % N_theta
-        i = (n - j)/N_theta
+        i = (n - j)/N_theta + 0.5
 
         r = i*self.dr
         theta = j*self.dtheta
@@ -43,8 +88,8 @@ class IO:
             "T": T, "u_r": u_r, "u_theta": u_theta,
         }
 
-        output_df = pd.DataFrame(output_dict, columns=self.IO_cols)
-        with open(self.IO_file, "a") as f:
+        output_df = pd.DataFrame(output_dict, columns=self.OX_cols)
+        with open(self.IO_file_snap, "a") as f:
             output_df.to_csv(f, header=False, index=False)
 
         return True

src/plot_functions.py
@@ -60,19 +60,40 @@ class animator:
         N_theta = self.N_theta
         R = self.R
         t_vals = data["t"].unique()
+        dt = np.hstack([0, np.diff(t_vals)])
         self.N_t = len(t_vals)
+
+        n = np.arange(N_r * N_theta)
+        j = n % N_theta
+        i = (n - j) / N_theta + 0.5
+        r = i * self.dr
+        N_t = len(t_vals)
 
         # Map data for theta = 0 to theta = 2*pi
         # to make the contour plots periodic
         X = np.vstack([self.X, self.X[0]])
         Y = np.vstack([self.Y, self.Y[0]])
 
+        T_old = data["T"].values.reshape(-1, N_r*N_theta)
         T = data["T"].values.reshape(-1, X.shape[1], X.shape[0]-1).T
         T = np.concatenate([T, T[0, :, :].reshape(1, T.shape[1], len(t_vals))], axis=0)
         T0 = T[:, :, 0]
 
-        v = np.sqrt(data["u_r"]**2 + data["u_theta"]**2)
-        v = np.log10(v.values.reshape(-1, X.shape[1], X.shape[0]-1).T)
+        # v = np.sqrt(data["u_r"]**2 + data["u_theta"]**2)
+        # v = np.log10(v.values.reshape(-1, X.shape[1], X.shape[0] - 1).T)
+        # v = v.values.reshape(-1, X.shape[1], X.shape[0]-1).T
+        kernels = self.kernels
+        u_r = data["u_r"].values.reshape(N_t, N_r*N_theta)
+        u_theta = data["u_theta"].values.reshape(N_t, N_r * N_theta)
+        div_u = []
+        for i in range(N_t):
+            div_u_r = u_r[i] / r + kernels["psi"]["grad_r"].dot(u_r[i])
+            div_u_theta = kernels["psi"]["grad_theta"].dot(u_theta[i])
+            mag_u = np.sqrt(u_r[i]**2 + u_theta[i]**2)
+            # div_u.append(self.beta*T_old[i]*(div_u_r + div_u_theta)*dt[i])
+            div_u.append((div_u_r + div_u_theta)/mag_u)
+        v = np.array(div_u).reshape(-1, X.shape[1], X.shape[0]-1).T
+        print(v.min(), v.max(), v.mean(), np.nanmedian(np.abs(v)))
         v = np.concatenate([v, v[0, :, :].reshape(1, v.shape[1], len(t_vals))], axis=0)
         v0 = v[:, :, 0]
 
@@ -90,12 +111,12 @@ class animator:
 
         # Model object perimeter
         circle = matplotlib.patches.Circle(
-            xy=[0, 0], radius=(self.N_r-1)*self.dr,
+            xy=[0, 0], radius=(self.N_r-0.5)*self.dr,
             edgecolor="k", facecolor="none"
         )
 
         circle2 = matplotlib.patches.Circle(
-            xy=[0, 0], radius=(self.N_r-2)*self.dr,
+            xy=[0, 0], radius=(self.N_r-1.5)*self.dr,
             edgecolor="k", facecolor="none"
         )
 
@@ -143,7 +164,7 @@ class animator:
 
         print("Rendering animation...")
 
-        duration = 20.0
+        duration = 10.0
         self.ani_dict = {
             "tmax": duration,
             "X": X,
@@ -157,7 +178,7 @@ class animator:
             "axes": (ax1, ax2),
         }
         ani = VideoClip(self.update_animation_save, duration=duration)
-        ani.write_videofile("movie.mp4", fps=30)
+        ani.write_videofile("movie.mp4", fps=T.shape[0]/duration)
 
         # plt.show()
         pass
@@ -171,7 +192,7 @@ class animator:
         R = self.R
 
         circle = matplotlib.patches.Circle(
-            xy=[0, 0], radius=(self.N_r-1)*self.dr,
+            xy=[0, 0], radius=(self.N_r-0.5)*self.dr,
             edgecolor="k", facecolor="none"
         )
 

src/polar_solver.py
@@ -4,13 +4,16 @@ from scipy.sparse.linalg import spsolve, inv
 from src.IO_lib import IO
 from src.plot_functions import animator
 from src.rk45 import rk45_solver
+from src.auxiliary import helper
+from time import time
 
 
-class FD_solver(animator, IO, rk45_solver):
+class FD_solver(animator, helper, IO, rk45_solver):
 
-    def __init__(self, IO_file="output.csv"):
+    def __init__(self):
         animator.__init__(self)
-        IO.__init__(self, IO_file=IO_file)
+        helper.__init__(self)
+        IO.__init__(self)
         rk45_solver.__init__(self)
         self.t_yr = 3600*24*365.25
         pass
@@ -18,9 +21,9 @@ class FD_solver(animator, IO, rk45_solver):
     def set_params(self, params):
         self.__dict__.update(params)
         self.dr = self.R / self.N_r
-        self.dtheta = 2 * np.pi / self.N_theta
+        self.dtheta = self.theta_range / self.N_theta
         self.dx_min = min(self.dr, self.dr*self.dtheta)
-        r = np.arange(self.N_r) * self.dr
+        r = (np.arange(self.N_r) + 0.5) * self.dr
         theta = np.arange(self.N_theta) * self.dtheta
         R, Theta = np.meshgrid(r, theta)
         self.X = R * np.cos(Theta)
@@ -36,7 +39,7 @@ class FD_solver(animator, IO, rk45_solver):
         T_ext = self.T_ext
 
         N_t = N_r * N_theta
-        i = np.arange(N_r)
+        i = np.arange(N_r) + 0.5
         i = np.repeat(i, N_theta)
         I = np.ones(N_t)
         i_max = N_theta * (N_r - 1)     # Exterior elements
@@ -123,10 +126,6 @@ class FD_solver(animator, IO, rk45_solver):
         a1[:N_theta] = 0.0
         a2[:N_theta] = 0.0
 
-        # Exterior B.C.: fixed T = T_ext
-        # This B.C. is negated by B.C. for psi:
-        # Radial flow u_r = 0, so that u_r dT/d_r = 0
-
         # Construct sparse matrix
         T_r = diags([a1[N_theta:], 0, a2[:-N_theta]], offsets=[-N_theta, 0, N_theta])
         kernels["T"]["grad_r"] = T_r
@@ -185,23 +184,20 @@ class FD_solver(animator, IO, rk45_solver):
 
         """ Construct stream function kernels (Laplacian) """
 
-        """ Construct psi_r matrix """
+        """ Construct psi_r / r matrix """
         # Central first differences stencil
         a1 = -1 / (2 * i * dr ** 2) * I
         a2 = -a1
         a3 = np.zeros_like(a1)
 
         # Interior B.C.: no flow
-        a1[:N_theta] = 0.0
-        a2[:N_theta] = 0.0
+        a1[:N_theta] *= 0
+        a3[:N_theta] = -1
 
-        # Exterior B.C.: free surface (d^2 theta / d r^2 = 0
-        a2[i_max:] = 0.0
-        a3[i_max:] = -2.0*a1[-1]
-
-        # Exterior B.C.: no flow
-        # a1[:N_theta] = 0.0
-        # a2[:N_theta] = 0.0
+        # Exterior B.C.: free surface (u_th^N+1 = u_th^N)
+        a1[i_max:] *= -1
+        a2[i_max:] *= 0
+        a3[i_max:] = 1
 
         # Construct sparse matrix
         # A = diags([a1[N_theta:], 0, a2[:-N_theta]], offsets=[-N_theta, 0, N_theta])
@@ -214,18 +210,15 @@ class FD_solver(animator, IO, rk45_solver):
         a3 = a1.copy()
 
         # Interior B.C.: no gradient --> x(i+1) = x(i-1)
-        # --> x(i-1) - 2x(i) + x(i+1) = 2x(i-1) - 2x(i)
-        a1[:N_theta] *= 0.0
-        a3[:N_theta] *= 2.0
-
-        # Exterior B.C.: no gradient --> x(i+1) = x(i-1)
         # --> x(i-1) - 2x(i) + x(i+1) = 2x(i+1) - 2x(i)
-        # a1[i_max:] *= 2.0
-        # a3[i_max:] *= 0.0
-        # Exterior B.C.: free surface
-        a1[i_max:] = 0.0
-        a2[i_max:] = 0.0
-        a3[i_max:] = 0.0
+        a1[:N_theta] *= 0
+        a2[:N_theta] *= (3.0 / 2.0)
+        a3[:N_theta] *= 3
+
+        # Exterior B.C.: free surface (u_th^N+1 = u_th^N)
+        a1[i_max:] *= 3
+        a2[i_max:] *= (3.0/2.0)
+        a3[i_max:] *= 0
 
         # Construct sparse matrix
         B = diags([a1[N_theta:], a2, a3[:-N_theta]], offsets=[-N_theta, 0, N_theta])
@@ -242,10 +235,11 @@ class FD_solver(animator, IO, rk45_solver):
         a3[:N_theta] = 0.0
 
         # Exterior B.C.: no radial flow for all j, which
-        # # implies that d^2 psi / d theta^2 = 0 (psi uniform)
+        # implies that d^2 psi / d theta^2 = 0 (psi uniform)
         a1[i_max:] = 0.0
         a2[i_max:] = 0.0
         a3[i_max:] = 0.0
+
 
         # Left periodic B.C: T[i, -1] = T[i, N_theta-1]
         a4 = np.zeros(N_t)
@@ -264,8 +258,6 @@ class FD_solver(animator, IO, rk45_solver):
 
         # Construct Laplacian = nabla^2 (note: not multiplied by alpha)
         laplacian_psi = A + B + C
-        # NOTE: using spsolve is faster than precomputing
-        # inverse (which is dense)
 
         kernels["psi"]["laplacian"] = laplacian_psi
 
@@ -275,6 +267,7 @@ class FD_solver(animator, IO, rk45_solver):
         # Central first differences stencil
         a1 = -1 / (2 * dr) * I
         a2 = -a1
+        a3 = np.zeros_like(a1)
 
         # Interior B.C.: no gradient
         a1[:N_theta] = 0.0
@@ -285,7 +278,7 @@ class FD_solver(animator, IO, rk45_solver):
         a2[i_max:] = 0.0
 
         # Construct sparse matrix
-        psi_r = diags([a1[N_theta:], 0, a2[:-N_theta]], offsets=[-N_theta, 0, N_theta])
+        psi_r = diags([a1[N_theta:], a3, a2[:-N_theta]], offsets=[-N_theta, 0, N_theta])
         kernels["psi"]["grad_r"] = psi_r
 
         """ Construct psi_theta matrix """
@@ -296,7 +289,7 @@ class FD_solver(animator, IO, rk45_solver):
         # Interior B.C.: identical elements
         a1[:N_theta] = 0.0
         a2[:N_theta] = 0.0
-
+        #
         # Exterior B.C.: no gradients
         a1[i_max:] = 0.0
         a2[i_max:] = 0.0
@@ -340,7 +333,7 @@ class FD_solver(animator, IO, rk45_solver):
 
         # Step 1: compute r_T_theta, solve for psi
         r_T_theta = kernels["T"]["r_grad_theta"].dot(T)
-        psi = spsolve(kernels["psi"]["laplacian"], self.dzeta * r_T_theta)
+        psi = spsolve(kernels["psi"]["laplacian"], self.dzeta * r_T_theta, permc_spec="MMD_AT_PLUS_A")
 
         # Step 2: compute velocity components = grad psi
         u_r = kernels["psi"]["grad_theta"].dot(psi)
@@ -366,7 +359,7 @@ class FD_solver(animator, IO, rk45_solver):
         self.i += 1
         self.t = t
 
-        if (self.i % self.N_IO == 0) or (self.i % self.N_print == 0):
+        if (self.i % self.N_IO_time == 0) or (self.i % self.N_IO_snap == 0) or (self.i % self.N_print == 0):
             kernels = self.kernels
 
             # Step 1: compute r_T_theta, solve for psi
@@ -378,13 +371,11 @@ class FD_solver(animator, IO, rk45_solver):
             u_theta = -kernels["psi"]["grad_r"].dot(psi)
 
             if self.i % self.N_print == 0:
-                print(
-                    "t = %.1f kyr \t dt = %.2e yr \t Tmax = %.1f K \t vmax = %.3e m/s" %
-                    (1e-3 * t / self.t_yr, dt / self.t_yr, T.max(),
-                     np.max(np.sqrt(u_r ** 2 + u_theta ** 2)))
-                )
-            if self.i % self.N_IO == 0:
-                self.write(t, T, u_r, u_theta)
+                self.screen_write(t, dt, T, u_r, u_theta)
+            if (self.i % self.N_IO_time == 0) and (self.IO_file_time is not None):
+                self.OT_file_write(t, T)
+            if (self.i % self.N_IO_snap == 0) and (self.IO_file_snap is not None):
+                self.OX_file_write(t, T, u_r, u_theta)
 
         pass
 
@@ -398,7 +389,12 @@ class FD_solver(animator, IO, rk45_solver):
 
         kernels = self.kernels
         dt = t - self.t
-        dx_min = self.dx_min
+        N_r, N_theta = self.N_r, self.N_theta
+
+        n = np.arange(N_r * N_theta)
+        j = n % N_theta
+        i = (n - j) / N_theta + 0.5
+        r = i * self.dr
 
         # Step 1: compute r_T_theta, solve for psi
         r_T_theta = kernels["T"]["r_grad_theta"].dot(T)
@@ -407,17 +403,29 @@ class FD_solver(animator, IO, rk45_solver):
         # Step 2: compute velocity components = grad psi
         u_r = kernels["psi"]["grad_theta"].dot(psi)
         u_theta = -kernels["psi"]["grad_r"].dot(psi)
-        u_mag = np.sqrt(u_r**2 + u_theta**2)
 
-        dt_crit = dx_min / np.max(u_mag)
-        if dt > dt_crit:
+        # Divergence terms of u
+        div_u_r = u_r / r + kernels["psi"]["grad_r"].dot(u_r)
+        div_u_theta = kernels["psi"]["grad_theta"].dot(u_theta)
+
+        # Find limiting time step due to advection
+        dt_r = np.nanmin(self.dr / np.abs(u_r))
+        dt_theta = np.nanmin(r * self.dtheta / np.abs(u_theta))
+        dt_crit = np.min([dt_r, dt_theta])
+
+        if dt > 0.9*dt_crit:
             self.dtmax = 0.9*dt_crit
 
         # Step 3: compute diffusive and advective terms
         dT_diff = self.alpha * kernels["T"]["laplacian"].dot(T)
         T_r = kernels["T"]["grad_r"].dot(T)
         T_theta = kernels["T"]["grad_theta"].dot(T)
-        dT_adv = self.beta * (u_r * T_r + u_theta * T_theta)
+
+        # Advective term includes non-solenoidal flow (div u =/= 0),
+        # owing to limited numerical accuracy.
+        dT_adv1 = self.beta * (u_r * T_r + u_theta * T_theta)
+        dT_adv2 = self.beta * T * (div_u_r + div_u_theta)
+        dT_adv = dT_adv1 + dT_adv2
 
         # Step 4: compute source function (including B.C.)
         O_base = kernels["O"]
@@ -433,13 +441,15 @@ class FD_solver(animator, IO, rk45_solver):
         self.construct_kernels()
         print("Kernels constructed")
 
-        self.create_IO_file()
+        self.create_IO_files()
         print("Output file created")
 
         print("Running simulation...")
         self.t = 0.0
+        self.tmax = tmax
         self.dtmax = tmax
         self.i = 0
+        self.t_wall_start = time()
 
         dt0 = 1e3
         gen = self.adaptive_solver(

test/cooling_benchmark.py
@@ -38,7 +38,7 @@ class AnalyticalSolution:
 
 """ Basic problem parameters """
 
-R = 40e3        # Radius [m]
+R = 45e3        # Radius [m]
 N = 40          # Number of mesh nodes in r
 alpha = 1.0e-4  # Thermal diffusivity [m2/s]
 
@@ -48,13 +48,14 @@ T_ext = 300.0   # External temperature [K]
 t_yr = 3600*24*365.25   # Number of seconds in 1 year
 
 # Mesh node positions
-r = np.linspace(0, R-R/N, N)
+dr = R / N
+r = np.linspace(0.5*dr, R-0.5*dr, N)
 
 # Mirror mesh (for symmetric plotting), set scale to km
 r_both = np.hstack([-r[::-1], r]) * 1e-3
 
 # Instantiate analytical solution class
-ana = AnalyticalSolution(alpha, R, T0-T_ext)
+ana = AnalyticalSolution(alpha, R+0.5*dr, T0-T_ext)
 
 
 """ Numerical simulation parameters """
@@ -73,19 +74,24 @@ params = {
     "dzeta": 0.0,
     "T_ext": T_ext,
     "t_half": 1.0,
-    "N_IO": 1000,
+    "N_IO_snap": 1000,
+    "N_IO_time": 1,
     "N_print": 100,
     "rtol": 1e-10,      # High integration precision
     "atol": 1e-10,
+    "IO_file_time": None,
+    "IO_file_snap": "cooling_benchmark_output.csv",
+    "theta_range": 2*np.pi,
 }
 
-solver = FD_solver(IO_file="cooling_benchmark_output.csv")
+solver = FD_solver()
 solver.set_params(params)
 
 T0 = np.ones(N_r*N_theta)*T0        # Initial temperature vector
 tmax = 50e3 * solver.t_yr           # Simulation duration
 # solver.run_simulation(T0, tmax)     # Run simulation
-data = solver.read()                # Read simulation output data
+solver.read()                       # Read simulation output data
+data = solver.ox_data
 
 t_sim = np.unique(data["t"])        # Simulation time data
 r_sim = np.unique(data["r"])        # Simulation mesh node positions
@@ -107,6 +113,7 @@ for i, ti in enumerate(t_sim):
 
     # Mirror and plot analytical solution
     T_both = np.hstack([T[::-1], T])
+
     plt.plot(r_both, T_both, c="k", ls="--")
 
     # Plot numerical solution

test/kernel_tests.py
@@ -6,9 +6,9 @@ from src.polar_solver import FD_solver
 
 """ Benchmark test parameters """
 
-R = 10e3            # Radius [m]
-N_r = 100           # Number of mesh nodes in r
-N_theta = 200       # Number of mesh nodes in theta
+R = 45e3           # Radius [m]
+N_r = 40           # Number of mesh nodes in r
+N_theta = 50       # Number of mesh nodes in theta
 
 params = {
     "N_r": N_r,
@@ -30,7 +30,7 @@ solver = FD_solver()
 solver.set_params(params)
 solver.construct_kernels()
 
-# Extract kernels (differentiation matrices)
+# Extract differentiation matrices
 kernels = solver.kernels
 
 # Extract cartesian coordinates
@@ -47,7 +47,7 @@ dr = R / N_r
 dtheta = 2*np.pi / N_theta
 
 # Cylindrical coordinates of mesh nodes
-r = i*dr
+r = (i+0.5)*dr
 theta = j*dtheta
 
 # Precompute common factor
@@ -77,29 +77,29 @@ T_laplace_kernel = kernels["T"]["laplacian"].dot(T)
 
 # Structure results
 results = {
-    r"$\nabla_r T$": {
+    r"$\nabla_r S$": {
         "analytical": T_r,
         "numerical": T_r_kernel,
         "range": (-2*T0/R, 2*T0/R),
     },
-    r"$\nabla_{\theta} T$": {
+    r"$\nabla_{\theta} S$": {
         "analytical": T_theta,
         "numerical": T_theta_kernel,
         "range": (-2*T0/R, 2*T0/R),
     },
-    r"$r \times \nabla_{\theta} T$": {
+    r"$r \times \nabla_{\theta} S$": {
         "analytical": rT_theta,
         "numerical": rT_theta_kernel,
         "range": (-T0, T0),
     },
-    r"$\nabla^2 T$": {
+    r"$\nabla^2 S$": {
         "analytical": T_laplace,
         "numerical": T_laplace_kernel,
         "range": (-2*T0/R**2, 2*T0/R**2),
     },
 }
 
-result_list = (r"$\nabla_r T$", r"$\nabla_{\theta} T$", r"$r \times \nabla_{\theta} T$", r"$\nabla^2 T$")
+result_list = (r"$\nabla_r S$", r"$\nabla_{\theta} S$", r"$r \times \nabla_{\theta} S$", r"$\nabla^2 S$")
 N_results = len(result_list)
 
 
@@ -114,35 +114,42 @@ for i, key in enumerate(result_list):
     num_res = result["numerical"].reshape((N_r, N_theta)).T
     num_res = np.vstack([num_res, num_res[0]])
 
+    # Diff excludes boundary regions
+    diff = np.abs((num_res[1:-1] - ana_res[1:-1]) / ana_res[1:-1])
+    print(np.mean(diff[np.isfinite(diff)]))
+
     amin, amax = result["range"]
     levels = np.linspace(amin, amax, 200)
 
     circle = matplotlib.patches.Circle(
         xy=[0, 0], radius=(N_r - 1) * dr,
-        edgecolor="k", facecolor="none", alpha=0.5
+        edgecolor="k", facecolor="none", alpha=0.5, zorder=100
     )
 
     circle2 = matplotlib.patches.Circle(
         xy=[0, 0], radius=(N_r - 1) * dr,
-        edgecolor="k", facecolor="none", alpha=0.5
+        edgecolor="k", facecolor="none", alpha=0.5, zorder=100
     )
 
     ax = plt.subplot(2, N_results, 1+i, aspect="equal")
+    plt.contour(X, Y, ana_res, levels, cmap="coolwarm", vmin=amin, vmax=amax, lw=0.1)
     plt.contourf(X, Y, ana_res, levels, cmap="coolwarm", vmin=amin, vmax=amax)
     ax.add_patch(circle)
     plt.axis("off")
     plt.xlim((-1.01 * R, 1.01 * R))
     plt.ylim((-1.01 * R, 1.01 * R))
-    plt.title(r"Analytical %s" % key)
+    plt.title(r"Analytical %s" % key, fontsize=14)
 
     ax = plt.subplot(2, N_results, 1+N_results+i, aspect="equal")
+    plt.contour(X, Y, num_res, levels, cmap="coolwarm", vmin=amin, vmax=amax, lw=0.1)
     plt.contourf(X, Y, num_res, levels, cmap="coolwarm", vmin=amin, vmax=amax)
     ax.add_patch(circle2)
     plt.axis("off")
     plt.xlim((-1.01 * R, 1.01 * R))
     plt.ylim((-1.01 * R, 1.01 * R))
-    plt.title(r"Numerical %s" % key)
+    plt.title(r"Numerical %s" % key, fontsize=14)
 
 plt.tight_layout()
+plt.subplots_adjust(left=0.02, right=0.98)
 plt.show()
 